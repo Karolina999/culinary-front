9c30d855b1b2815dfc46741e22da4f39aae4c116
Karolina Pierlak <kp055941@student.ath.edu.pl>
2022-11-04 21:47:27 +0100

    background-sm

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,32 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import styles from "../styles/login.module.css";
 
 const LogIn = () => {
   return (
     <div className={styles.background}>
-      <Container className="py-5">
+      <Container className="py-5 my-5">
         <Row className="justify-content-center">
-          <Col md={7}>
-            <h3 className="bold text-center pb-4">Logowanie</h3>
+          <Col md={6}>
+            <h3 className="bold text-center pb-5 pt-sm-5 pt-md-0 mt-5 mt-md-0">
+              Logowanie
+            </h3>
+            <Form>
+              <Form.Group className="mb-4">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="example@gmail.com" />
+              </Form.Group>
+
+              <Form.Group className="mb-4">
+                <Form.Label>Hasło</Form.Label>
+                <Form.Control type="password" placeholder="hasło" />
+              </Form.Group>
+              <div className="d-grid">
+                <Button variant="danger" type="submit" className="mt-3">
+                  Zaloguj się
+                </Button>
+              </div>
+            </Form>
           </Col>
         </Row>
       </Container>
