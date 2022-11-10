1c2715aa35dc654c25d9fcb8f03b71c487bdd30b
Karolina Pierlak <kp055941@student.ath.edu.pl>
2022-11-10 15:08:01 +0100

    add

--- pages/dodaj.tsx
+++ pages/dodaj.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Container, Col, Row, Form, Button } from "react-bootstrap";
 import SelectList from "../components/inputs/selectList";
+import { BsTrashFill } from "react-icons/bs";
 
 const Dodaj = () => {
   const options = [
@@ -12,11 +13,11 @@ const Dodaj = () => {
   return (
     <div>
       <Container className="py-5 d-flex justify-content-center">
-        <Col xs={12} lg={10} xxl={8}>
+        <Col xs={12} lg={10} xxl={9}>
           <h3 className="bold pb-4">Dodaj przepis</h3>
           <Form>
             <Form.Group className="mb-4">
-              <Form.Label>Nazwa potrawy</Form.Label>
+              <Form.Label className="bold">Nazwa potrawy</Form.Label>
               <Form.Control
                 placeholder="Zupa pomidorowa"
                 name=""
@@ -29,7 +30,7 @@ const Dodaj = () => {
               </Form.Control.Feedback> */}
             </Form.Group>
             <Form.Group className="mb-4">
-              <Form.Label>Kategoria</Form.Label>
+              <Form.Label className="bold">Kategoria</Form.Label>
               <Form.Select
                 // placeholder="Zupa pomidorowa"
                 name=""
@@ -44,16 +45,63 @@ const Dodaj = () => {
               </Form.Control.Feedback> */}
             </Form.Group>
             <Form.Group className="mb-4">
-              <Form.Label>Składniki</Form.Label>
+              <Form.Label className="bold">Stopień trudności</Form.Label>
+              <Form.Select
+                // placeholder="Zupa pomidorowa"
+                name=""
+                // value={values.email}
+                // onChange={handleChange}
+                // isInvalid={!!errors.email}
+              >
+                <option>Default select</option>
+              </Form.Select>
+              {/* <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback> */}
+            </Form.Group>
+            <Form.Group className="mb-4">
+              <Form.Label className="bold">Liczba osób</Form.Label>
+              <Form.Select
+                // placeholder="Zupa pomidorowa"
+                name=""
+                // value={values.email}
+                // onChange={handleChange}
+                // isInvalid={!!errors.email}
+              >
+                <option>Default select</option>
+              </Form.Select>
+              {/* <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback> */}
+            </Form.Group>
+            <Form.Group className="mb-4">
+              <Form.Label className="bold">Czas przygotowania</Form.Label>
+              <Form.Select
+                // placeholder="Zupa pomidorowa"
+                name=""
+                // value={values.email}
+                // onChange={handleChange}
+                // isInvalid={!!errors.email}
+              >
+                <option>Default select</option>
+              </Form.Select>
+              {/* <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback> */}
+            </Form.Group>
+
+            <Form.Group className="mb-4">
+              <Form.Label className="bold">Składniki</Form.Label>
               <Row>
-                <Col>
+                <Col xs={12} lg={7} className="mb-4 mb-lg-0">
                   <SelectList options={options} placeholder="Pomidory" />
                 </Col>
                 <Col>
                   <Row>
-                    <Col>
+                    <Col xs={4}>
                       <Form.Control
-                        placeholder="Zupa pomidorowa"
+                        type="number"
+                        placeholder="1"
                         name=""
                         // value={values.email}
                         // onChange={handleChange}
@@ -61,20 +109,82 @@ const Dodaj = () => {
                       />
                     </Col>
                     <Col>
-                      <Form.Control
-                        placeholder="Zupa pomidorowa"
+                      <Form.Select
+                        // placeholder="Zupa pomidorowa"
                         name=""
                         // value={values.email}
                         // onChange={handleChange}
                         // isInvalid={!!errors.email}
-                      />
+                      >
+                        <option>Default select</option>
+                      </Form.Select>
                     </Col>
                     <Col xs="auto">
-                      <Button>Kosz</Button>
+                      <Button variant="link" className="px-0">
+                        <BsTrashFill
+                          style={{ fontSize: "22px" }}
+                          className="text-danger"
+                        />
+                      </Button>
                     </Col>
                   </Row>
                 </Col>
               </Row>
+              <div className="d-flex pt-4">
+                <Button variant="success" className="ms-auto">
+                  Dodaj składnik
+                </Button>
+              </div>
+            </Form.Group>
+            <Form.Group className="mb-4">
+              <Form.Label className="bold">Przygotowanie</Form.Label>
+              <Row className="align-items-center">
+                <Col xs="12">
+                  <Form.Label>Krok 1</Form.Label>
+                </Col>
+                <Col>
+                  <Form.Control
+                    as="textarea"
+                    rows={1}
+                    placeholder="Zupa pomidorowa"
+                    name=""
+                    // value={values.email}
+                    // onChange={handleChange}
+                    // isInvalid={!!errors.email}
+                  />
+                  {/* <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback> */}
+                </Col>
+                <Col xs={12} md="auto">
+                  <Row className="pt-2 pt-md-0">
+                    <Col xs="auto" className="ms-auto">
+                      <Button variant="success">Dodaj zdjęcie</Button>
+                    </Col>
+                    <Col xs="auto">
+                      <Button variant="link" className="px-0">
+                        <BsTrashFill
+                          style={{ fontSize: "22px" }}
+                          className="text-danger"
+                        />
+                      </Button>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className="mb-4">
+              <Form.Label className="bold">Nazwa potrawy</Form.Label>
+              <Form.Control
+                type="file"
+                name=""
+                // value={values.email}
+                // onChange={handleChange}
+                // isInvalid={!!errors.email}
+              />
+              {/* <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback> */}
             </Form.Group>
           </Form>
         </Col>
